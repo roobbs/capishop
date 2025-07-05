8f91cd2eed31fcd76321c45c92df3a491f6d14d9
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-07-04 21:25:13 -0600

    feat: nav bar now recognizes path name to highlight correct button

--- app/components/BottomNavBar.tsx
+++ app/components/BottomNavBar.tsx
@@ -1,28 +1,42 @@
-import { Grid3X3, Heart, Menu } from "lucide-react";
+"use client";
+
+import { Circle, Grid3X3, Heart, Menu } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function BottomNavBar() {
+  const pathname = usePathname();
+
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-slate-400/90 px-4 py-3">
       <div className="flex items-center justify-around">
-        <Link href="/" className="flex flex-col items-center text-blue-600">
-          <div className="w-6 h-6 bg-blue-600 rounded-full flex items-center justify-center mb-1">
-            <div className="w-2 h-2 bg-white rounded-full"></div>
-          </div>
-          <span className="text-xs font-medium">Inicio</span>
+        <Link
+          href="/"
+          className={`flex gap-1 flex-col items-center ${
+            pathname === "/" ? "text-blue-600 font-bold" : "text-slate-400"
+          }`}
+        >
+          <Circle className="" />
+          <span className="text-xs ">Inicio</span>
         </Link>
         <Link
           href="/categories"
-          className="flex flex-col items-center text-slate-400"
+          className={`flex gap-1 flex-col items-center ${
+            pathname === "/categories"
+              ? "text-blue-600 font-bold"
+              : "text-slate-400"
+          }`}
         >
-          <div className="w-6 h-6 flex items-center justify-center mb-1">
-            <Grid3X3 className="" />
-          </div>
+          <Grid3X3 className="" />
           <span className="text-xs">Categorías</span>
         </Link>
         <Link
           href="/favorites"
-          className="flex flex-col items-center text-slate-400"
+          className={`flex gap-1 flex-col items-center ${
+            pathname === "/favorites"
+              ? "text-blue-600 font-bold"
+              : "text-slate-400"
+          }`}
         >
           <div className="w-6 h-6 flex items-center justify-center mb-1 relative">
             <Heart className="" />
@@ -32,11 +46,11 @@ export default function BottomNavBar() {
         </Link>
         <Link
           href="/menu"
-          className="flex flex-col items-center text-slate-400"
+          className={`flex gap-1 flex-col items-center ${
+            pathname === "/menu" ? "text-blue-600 font-bold" : "text-slate-400"
+          }`}
         >
-          <div className="w-6 h-6 flex items-center justify-center mb-1 relative">
-            <Menu className="" />
-          </div>
+          <Menu className="" />
           <span className="text-xs">Más</span>
         </Link>
       </div>
