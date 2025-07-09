9cacc7b34574f77cb750bc4a7f0466986466b571
Carlos S. <carlossalmeroncabrera@gmail.com>
2025-07-08 21:48:19 -0600

    style: updated nav bar style to show border and improve touchable area

--- app/ui/layout/BottomNavBar.tsx
+++ app/ui/layout/BottomNavBar.tsx
@@ -8,52 +8,57 @@ export default function BottomNavBar() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-slate-400/90 px-4 py-3">
-      <div className="flex items-center justify-around">
-        <Link
-          href="/"
-          className={`flex gap-1 flex-col items-center ${
-            pathname === "/" ? "text-blue-600 font-bold" : "text-slate-400"
-          }`}
-        >
-          <Circle className="" />
-          <span className="text-xs ">Inicio</span>
-        </Link>
-        <Link
-          href="/categories"
-          className={`flex gap-1 flex-col items-center ${
-            pathname === "/categories"
-              ? "text-blue-600 font-bold"
-              : "text-slate-400"
-          }`}
-        >
-          <Grid3X3 className="" />
-          <span className="text-xs">Categorías</span>
-        </Link>
-        <Link
-          href="/favorites"
-          className={`flex gap-1 flex-col items-center ${
-            pathname === "/favorites"
-              ? "text-blue-600 font-bold"
-              : "text-slate-400"
-          }`}
-        >
-          <div className="w-6 h-6 flex items-center justify-center mb-1 relative">
-            <Heart className="" />
-            <div className="absolute -top-1 -right-1 w-3 h-3 bg-blue-600 rounded-full"></div>
-          </div>
-          <span className="text-xs">Favoritos</span>
-        </Link>
-        <Link
-          href="/menu"
-          className={`flex gap-1 flex-col items-center ${
-            pathname === "/menu" ? "text-blue-600 font-bold" : "text-slate-400"
-          }`}
-        >
-          <Menu className="" />
-          <span className="text-xs">Más</span>
-        </Link>
-      </div>
+    <nav className="fixed grid grid-cols-4 bottom-0 left-0 right-0 bg-white">
+      <Link
+        href="/"
+        className={`flex gap-1 flex-col border-t py-3 items-center ${
+          pathname === "/"
+            ? "text-blue-600 font-bold border-t-2 border-blue-600"
+            : "text-slate-400 border-slate-400/90"
+        }`}
+      >
+        <Circle className="size-7" />
+        <span className="text-xs ">Inicio</span>
+      </Link>
+
+      <Link
+        href="/categories"
+        className={`flex gap-1 flex-col border-t py-3 items-center ${
+          pathname === "/categories"
+            ? "text-blue-600 font-bold border-t-2 border-blue-600"
+            : "text-slate-400 border-slate-400/90"
+        }`}
+      >
+        <Grid3X3 className="size-7" />
+        <span className="text-xs">Categorías</span>
+      </Link>
+
+      <Link
+        href="/favorites"
+        className={`flex gap-1 flex-col border-t py-3 items-center ${
+          pathname === "/favorites"
+            ? "text-blue-600 font-bold border-t-2 border-blue-600"
+            : "text-slate-400 border-slate-400/90"
+        }`}
+      >
+        <div className="w-6 h-6 flex items-center justify-center mb-1 relative">
+          <Heart className="size-7" />
+          <div className="absolute -top-1 -right-1 w-3 h-3 bg-blue-600 rounded-full"></div>
+        </div>
+        <span className="text-xs">Favoritos</span>
+      </Link>
+
+      <Link
+        href="/menu"
+        className={`flex gap-1 flex-col border-t py-3 items-center ${
+          pathname === "/menu"
+            ? "text-blue-600 font-bold border-t-2 border-blue-600"
+            : "text-slate-400 border-slate-400/90"
+        }`}
+      >
+        <Menu className="size-7" />
+        <span className="text-xs">Más</span>
+      </Link>
     </nav>
   );
 }
